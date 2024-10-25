606b048473520a91f5f675230a73de95d7bec20d
majko188 <marekbk@gmail.com>
2024-10-25 12:29:37 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const mysql = require('mysql2');
+const path = require('path'); // Import pre path
 const app = express();
 const port = 3000;
 
