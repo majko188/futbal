6cfb1ebf9ee6e9edb54d05d507850572b1d503e2
majko188 <marekbk@gmail.com>
2024-10-25 12:06:51 +0200

    Update database.js

--- backend/database.js
+++ backend/database.js
@@ -1,4 +1,11 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: 'sql7
+  host: 'sql7.freemysqlhosting.net',
+  user: 'sql7740631',
+  password: 'P4YdmNmYyr',
+  database: 'sql7740631',
+  port: 3306
+});
+
+module.exports = db; // Exportuj pripojenie na databázu
