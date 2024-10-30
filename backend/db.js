1e214b521e33ee14b5d224dd330e92d0256e31f7
Marek Bagi-Kovac <Marek.Bagi-Kovac@erstegroup.com>
2024-10-30 18:36:01 +0100

    small updates

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'yourUsername',
-    password: 'yourPassword',
-    database: 'futbal'
+    host: 'sql7.freemysqlhosting.net',
+    user: 'sql7740631',
+    password: 'P4YdmNmYyr',
+    database: 'sql7740631',
+    port: 3306
 });
 
 db.connect((err) => {
