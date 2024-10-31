37774c943881351a2021b87abc59629a0ba1c24a
Marek Bagi-Kovac <Marek.Bagi-Kovac@erstegroup.com>
2024-10-31 11:18:13 +0100

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: 'DB_HOST',
+    host: DB_HOST,
     user: 'sql7740631',
     password: 'P4YdmNmYyr',
     database: 'sql7740631',
