f904c528d6416b01d1d617f43a8727b824b5f083
Marek Bagi-Kovac <Marek.Bagi-Kovac@erstegroup.com>
2024-10-31 11:34:02 +0100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const pollRoutes = require('./routes/poll');
 const financeRoutes = require('./routes/finance');
 const adminRoutes = require('./routes/admin');
 const path = require('path');
+const dotenev = require('dotenv').config();
 const app = express();
 const port = 3000;
 
